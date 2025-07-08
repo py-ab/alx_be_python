56c77a90d8da82de2917b4460a520bd4d0ee4b12
Abdi <abdiofficial1934@gmail.com>
2025-07-08 12:24:06 -0700

    Add future age calculator script

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Get the user's current age
+current_age = int(input("How old are you? "))
+
+# Calculate the age in 2050 (2050 - 2023 = 27 years from now)
+age_in_2050 = current_age + 27
+
+# Print the result
+print(f"In 2050, you will be {age_in_2050} years old.")
